c2adb201b95dfa048ce02262291b9f8486064e97
mirsahib <mirsahib24@gmail.com>
2020-07-24 19:07:13 +0600

    :rocket: Add production env

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,11 +1,11 @@
 const prod = {
     url: {
-     API_URL: 'https://myapp.herokuapp.com',
-     API_URL_USERS: 'https://myapp.herokuapp.com/users'}
+     API_URL: 'https://frozen-sierra-86813.herokuapp.com/',
+     API_URL_USERS: 'https://frozen-sierra-86813.herokuapp.com/'}
    };
 const dev = {
     url: {
-        API_URL: 'http://127.0.0.1:8000/'
+        API_URL: 'https://frozen-sierra-86813.herokuapp.com/',
     }
 };
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
